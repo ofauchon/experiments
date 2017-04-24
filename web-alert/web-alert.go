0fc4f22b6a3f62a849e07ef4106c7be19061b531
Olivier Fauchon <olivier@oflabs.com>
2017-04-24 21:47:14 +0200

    Go fmt; added alert1.sleep configuration parameter

--- web-alert/web-alert.go
+++ web-alert/web-alert.go
@@ -5,15 +5,14 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"time"
-	"net/http"
 
-	"gopkg.in/gomail.v2"
 	"github.com/benbjohnson/phantomjs"
 	"github.com/spf13/viper"
+	"gopkg.in/gomail.v2"
 )
-
 
 //
 // Download page content
@@ -62,7 +61,7 @@ func shot(pUrl string) error {
 		return err
 	}
 
-//    page.SetClipRect(rectJson{Top:0, Left:0, Width: 1024, Height: 1024})
+	//    page.SetClipRect(rectJson{Top:0, Left:0, Width: 1024, Height: 1024})
 
 	if err := page.Render(".temp.png", "png", 25); err != nil {
 		return err
@@ -74,48 +73,44 @@ func shot(pUrl string) error {
 //
 // sendMail
 //
-func sendMail(pSub, pMsg, pTo, pFrom , pUrl string) {
-
+func sendMail(pSub, pMsg, pTo, pFrom, pUrl string) {
 
 	m := gomail.NewMessage()
 
-    m.SetHeader("From", pFrom);
-    m.SetHeader("To", pTo);
-    m.SetHeader("Subject", pSub);
-    m.Embed(".temp.png");
-    m.SetBody("text/html",`<a href="` + pUrl + `">Lien Original</a><img src="cid:.temp.png" alt="My image" />`) 
-    d := gomail.NewDialer(viper.GetString("smtp.host"), viper.GetInt("smtp.port") , viper.GetString("smtp.user"), viper.GetString("smtp.pass")  )
-    if err := d.DialAndSend(m); err != nil {
-        panic(err)
-    }
+	m.SetHeader("From", pFrom)
+	m.SetHeader("To", pTo)
+	m.SetHeader("Subject", pSub)
+	m.Embed(".temp.png")
+	m.SetBody("text/html", `<a href="`+pUrl+`">Lien Original</a><img src="cid:.temp.png" alt="My image" />`)
+	d := gomail.NewDialer(viper.GetString("smtp.host"), viper.GetInt("smtp.port"), viper.GetString("smtp.user"), viper.GetString("smtp.pass"))
+	if err := d.DialAndSend(m); err != nil {
+		panic(err)
+	}
 }
 
 func main() {
-
 
 	log.Println("Start Alert System")
 	viper.SetConfigName("web-alert")
 	viper.AddConfigPath("$HOME/.config")
 	viper.AddConfigPath("./config")
 
-     err := viper.ReadInConfig()
-  	if err != nil {
-    	fmt.Println("Config file not found...")
-        os.Exit(1)
-  	} 
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println("Config file not found...")
+		os.Exit(1)
+	}
 	fmt.Println("Config file found ...")
 
-	cUrl:=viper.GetString("alert1.url")
-	cFrom:=viper.GetString("alert1.from")
-	cTo:=viper.GetString("alert1.to")
-	cSubject:=viper.GetString("alert1.subject")
+	cUrl := viper.GetString("alert1.url")
+	cFrom := viper.GetString("alert1.from")
+	cTo := viper.GetString("alert1.to")
+	cSubject := viper.GetString("alert1.subject")
 
-    fmt.Println("url:",cUrl, " subject: ",cSubject," from:",cFrom," to:",cTo)
-
+	fmt.Println("url:", cUrl, " subject: ", cSubject, " from:", cFrom, " to:", cTo)
 
 	cur_md5 := md5.Sum([]byte{0})
 	old_md5 := md5.Sum([]byte{0})
-	pause := 60
 
 	for {
 		html := download(cUrl)
@@ -124,16 +119,16 @@ func main() {
 		if old_md5 != cur_md5 {
 			log.Println("MD5 Changed ", old_md5, cur_md5)
 			log.Println("Creating screenshot for page: ", cUrl)
-			shot(cUrl);
-			log.Println("Sending mail from:",cFrom, " to:", cTo)
+			shot(cUrl)
+			log.Println("Sending mail from:", cFrom, " to:", cTo)
 			sendMail(cSubject, string(html), cTo, cFrom, cUrl)
-			old_md5=cur_md5
+			old_md5 = cur_md5
 
 		} else {
 			log.Println("MD5 did not change")
 		}
-		log.Println("Going to sleep for   ", pause, " seconds")
-		time.Sleep(time.Second * 300)
+		log.Println("Going to sleep for   ",viper.GetInt("alert1.sleep") , " seconds")
+		time.Sleep(time.Second * time.Duration(viper.GetInt("alert1.sleep")) )
 	}
 
 }
